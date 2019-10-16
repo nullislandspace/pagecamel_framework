043b451cd0b73211334d9b3cd1cd8efa41a65292
Rene Schickbauer <cavac@blackmesa>
2019-10-16 17:18:46 +0200

    Centralized and open sourced the print processor from project earthrise

---
+++ lib/PageCamel/Helpers/PrintProcessor.pm
@@ -0,0 +1,432 @@
+package PageCamel::Helpers::PrintProcessor;
+#---AUTOPRAGMASTART---
+use 5.020;
+use strict;
+use warnings;
+use diagnostics;
+use mro 'c3';
+use English qw(-no_match_vars);
+use Carp;
+our $VERSION = 2.3;
+use Fatal qw( close );
+use Array::Contains;
+#---AUTOPRAGMAEND---
+
+use GD;
+use GD::Text;
+use PageCamel::Helpers::FileSlurp qw(writeBinFile);
+use PageCamel::Helpers::DataBlobs;
+use PageCamel::Helpers::TestData;
+
+sub new {
+    my ($proto, $config) = @_;
+    my $class = ref($proto) || $proto;
+    
+    my $self = bless $config, $class;
+
+    $self->{bitmapfont} = GD::Font->load($self->{font});
+    
+    return $self;
+}
+
+sub printStartDocument {
+    my ($self) = @_;
+    
+    $self->{img} = GD::Image->new($self->{width}, $self->{height});
+    $self->{imgoffs} = 0;
+    $self->{imgblack} = $self->{img}->colorAllocate(0, 0, 0);
+    $self->{imgwhite} = $self->{img}->colorAllocate(255, 255, 255);
+    
+    $self->{img}->filledRectangle(0, 0, $self->{width}, $self->{height}, $self->{imgwhite});
+    
+    return;
+}
+
+sub printEndDocument {
+    my ($self, $printername) = @_;
+    
+    # Need to downsize image to minimum required length
+    my $cropped = GD::Image->new($self->{width}, $self->{imgoffs});
+    my $black = $cropped->colorAllocate(0, 0, 0);
+    my $white = $cropped->colorAllocate(255, 255, 255);
+    
+    $cropped->copyResized($self->{img},
+                          0, 0, # DEST X Y
+                          0, 0, # SRC X Y
+                          $self->{width}, $self->{imgoffs}, # DEST W H
+                          $self->{width}, $self->{imgoffs}, # SRC W H
+                          );
+    
+    my $ofname = $self->makeFName();
+
+    my $imagedata = $cropped->png;
+    writeBinFile($ofname, $imagedata);
+    writeBinFile('/home/cavac/temp/bla.png', $self->{img}->png);
+    
+    my $cmd = $self->{printcommand};
+    if(defined($printername) && $printername ne '') {
+        $cmd .= ' -d ' . $printername;
+    }
+    $cmd .= ' ' . $ofname;
+    `$cmd`;
+    
+    unlink $ofname;
+    
+    delete $self->{img};
+    delete $self->{imgoffs};
+    delete $self->{imgblack};
+    delete $self->{imgwhite};
+    return $imagedata;
+}
+
+sub printAddTextLine {
+    my ($self, $line) = @_;
+    
+    chomp $line;
+    
+    $self->{img}->string($self->{bitmapfont}, 10, $self->{imgoffs}, $line, $self->{imgblack});
+    
+    $self->{imgoffs} += 16;
+    
+    return;
+}
+
+sub printAddBigTextLine {
+    my ($self, $line) = @_;
+    
+    chomp $line;
+    
+    $self->{img}->stringFT($self->{imgblack}, $self->{bigfont}, 50, 0, 10, $self->{imgoffs} + 50, $line);
+    
+    $self->{imgoffs} += 55;
+    
+    return;
+}
+
+sub printAddImage {
+    my ($self, $filename, $isbindata) = @_;
+    
+    my $pic;
+    if($isbindata) {
+        $pic = GD::Image->newFromPngData($filename, 0);
+    } else {
+        $pic = GD::Image->newFromPng($filename, 0);
+    }
+    
+    my ($w, $h) = $pic->getBounds();
+    
+    my $destw = $self->{width};
+    my $scale = $w / $destw;
+    my $desth = int($h / $scale);
+    
+    $self->{img}->copyResized($pic,
+                              0, $self->{imgoffs}, # DEST X Y
+                              0, 0, # SRC X Y
+                              $destw, $desth, # DEST W H
+                              $w, $h, # SRC W H
+                              );
+    
+    $self->{imgoffs} += 10 + $desth;
+    
+    return;
+}
+
+sub printAddGreyscaleImage {
+    my ($self, $filename, $isbindata) = @_;
+    
+    my $rawpic;
+    if($isbindata) {
+        $rawpic = GD::Image->newFromPngData($filename, 0);
+    } else {
+        $rawpic = GD::Image->newFromPng($filename, 0);
+    }
+        
+    my ($w, $h) = $rawpic->getBounds();
+    
+    my $destw = $self->{width};
+    my $scale = $w / $destw;
+    my $desth = int($h / $scale);
+    
+    my $pic = GD::Image->new($destw, $desth);
+    
+    # Copy palette
+    my $colorcount = $rawpic->colorsTotal;
+    for(my $c = 0; $c < $colorcount; $c++) {
+        my ($r,$g,$b) = $rawpic->rgb($c);
+        $pic->colorAllocate($r, $g, $b);
+    }
+    
+    $pic->copyResized($rawpic,
+                      0, 0, # DEST X Y
+                      0, 0, # SRC X Y
+                      $destw, $desth, # DEST W H
+                      $w, $h, # SRC W H
+                    );
+    
+    my @rawgreys = (
+        '0000000000000000',
+        '0000000001000000',
+        '0000100000100000',
+        '0010000001000001',
+        '1000001000101000',
+        '1010000000010110',
+        '0001010001101010',
+        '1010110010100100',
+        '1010010101101010',
+        '1001011001011011',
+        '1001011110101101',
+        '1101101001111110',
+        '1011111001111011',
+        '1011011111101111',
+        '1111011111101111',
+        '1111111110111111',
+        '1111111111111111',
+    );
+    my $levels = scalar @rawgreys;
+    my $bitlen = length($rawgreys[0]);
+    
+    my @greys;
+    foreach my $rawgrey (@rawgreys) {
+        my @parts = split//, $rawgrey;
+        push @greys, \@parts;
+    }
+    
+    for(my $y = 0; $y < $desth; $y++) {
+        for(my $x = 0; $x < $destw; $x++) {    
+            my $index = $pic->getPixel($x, $y);
+            my ($r,$g,$b) = $pic->rgb($index);
+            my $greypixel = int(($r+$g+$b)/3);
+            my $level = int($greypixel / (255 / $levels));
+            
+            my $offs = int(rand($bitlen));
+            my $bit = $greys[$level]->[($x + $offs) % $bitlen];
+            
+            if(!$bit) {
+                $self->{img}->setPixel($x, $y + $self->{imgoffs}, $self->{imgblack});
+            }
+        }
+    }
+    
+    $self->{imgoffs} += $desth;
+    return;
+}
+
+sub rememberPrint {
+    my ($self, $imagedata, $description) = @_;
+    
+    my $dbh = $self->{dbh};
+    my $reph = $self->{reph};
+    
+    my $blob = PageCamel::Helpers::DataBlobs->new($dbh);
+    $blob->blobOpen();
+    $blob->blobWrite(\$imagedata);
+    my $filesize = $blob->getLength();
+    my $blobid = $blob->blobID();
+    $blob->blobClose();
+    
+    my $inssth = $dbh->prepare_cached("INSERT INTO " . $self->{table} . "
+                                      (file_datablob_id, filesize_bytes, description)
+                                      VALUES (?, ?, ?)
+                                      RETURNING document_id")
+            or croak($dbh->errstr);
+    if(!$inssth->execute($blobid, $filesize, $description)) {
+        $reph->debuglog("DB FAIL: " . $dbh->errstr);
+        $dbh->rollback;
+        return 0;
+    }
+    my $line = $inssth->fetchrow_hashref;
+    $inssth->finish;
+    my $documentid = $line->{document_id};
+    
+    $reph->debuglog("Printed document saved as ID $documentid with BLOB $blobid");
+    $dbh->commit;
+    
+    return 1;
+}
+
+sub reprintDocument {
+    my ($self, $documentid, $printername) = @_;
+
+    my $dbh = $self->{dbh};
+    my $reph = $self->{reph};
+    
+    $reph->debuglog("REPRINT ID $documentid on printer $printername");
+    
+    my $selsth = $dbh->prepare_cached("SELECT * FROM " . $self->{table} . "
+                                       WHERE document_id = ?")
+            or croak($dbh->errstr);
+    if(!$selsth->execute($documentid)) {
+        $reph->debuglog("   FAIL: " . $dbh->errstr);
+        $dbh->rollback;
+        return;
+    }
+    
+    
+    my $line = $selsth->fetchrow_hashref;
+    $selsth->finish;
+    
+    if(!defined($line) || !defined($line->{document_id})) {
+        $reph->debuglog("   Document not found");
+        $dbh->rollback;
+        return;
+    }
+    
+    my $imagedata;
+    my $blob = PageCamel::Helpers::DataBlobs->new($dbh, $line->{file_datablob_id});
+    my $len = $blob->getLength();
+    $blob->blobRead(\$imagedata);
+    $blob->blobClose();
+    $dbh->commit;
+    
+    my $ofname = makeFName();
+    print STDERR $ofname, "\n";
+
+    writeBinFile($ofname, $imagedata);
+    
+    my $cmd = $self->{printcommand};
+    if(defined($printername) && $printername ne '') {
+        $cmd .= ' -d ' . $printername;
+    }
+    $cmd .= ' ' . $ofname;
+    `$cmd`;
+    
+    $reph->debuglog("    " . $line->{description});
+    
+    unlink $ofname;
+    
+    return;
+}
+
+sub printAddTestPattern_HeatupCooldown {
+    my ($self) = @_;
+    
+    for(1..3) {
+        $self->{img}->filledRectangle(0, $self->{imgoffs},
+                                      $self->{width}, $self->{imgoffs} + 200,
+                                              $self->{imgblack});
+        $self->{imgoffs} += 400;
+    }
+    
+    return
+}
+
+sub printAddTestPattern_VerticalLines {
+    my ($self, $pointsize) = @_;
+    
+    for(my $x = 0; $x < $self->{width}; $x += $pointsize) {
+
+        if($x % ($pointsize * 2) != 0) {
+            next;
+        }
+        
+        $self->{img}->filledRectangle($x, $self->{imgoffs},
+                                      $x + $pointsize, $self->{imgoffs} + 40,
+                                          $self->{imgblack});
+    }
+
+    
+    $self->{imgoffs} += 40;
+    
+    return
+}
+
+sub printAddTestPattern_HorizontalLines {
+    my ($self, $pointsize) = @_;
+    
+    for(1..2) {
+        my $i = 0;
+
+        $self->{img}->filledRectangle(0, $self->{imgoffs}, $self->{width},
+                                          $self->{imgoffs} + $pointsize,
+                                          $self->{imgblack});
+        
+        $self->{imgoffs} += $pointsize * 2;
+        
+    }
+    
+    return
+}
+
+sub printTestMessage {
+    my ($self) = @_;
+    
+    my @lines = PageCamel::Helpers::TestData::getTestLines();
+    
+    $self->printStartDocument();
+    
+    if(1) {
+        $self->printAddTextLine("TEST 'Text'");
+        $self->printAddBigTextLine("GladOS");
+        foreach my $line (@lines) {
+            $self->printAddTextLine($line);
+        }
+        for(1..3) {
+            $self->printAddTextLine('');
+        }
+    }
+
+    if(1) {
+        $self->printAddTextLine("TEST 'Vertical lines'");
+        for(my $i = 20; $i > 0; $i--) {
+            $self->printAddTestPattern_VerticalLines($i);
+        }
+        for(1..3) {
+            $self->printAddTextLine('');
+        }
+    }
+
+    if(1) {
+        $self->printAddTextLine("TEST 'Horizontal lines'");
+        for(my $i = 20; $i > 0; $i--) {
+            $self->printAddTestPattern_HorizontalLines($i);
+        }
+        for(1..3) {
+            $self->printAddTextLine('');
+        }
+    }
+
+    if(1) {
+        
+        $self->printAddTextLine("TEST 'Heat up/Cooldown'");
+        $self->printAddTestPattern_HeatupCooldown();
+        
+    }
+    
+    if(1) {
+        $self->printAddTextLine("TEST 'Greyscale Images'");
+        $self->printAddGreyscaleImage(PageCamel::Helpers::TestData::getTestImage1(), 1);
+        $self->printAddGreyscaleImage(PageCamel::Helpers::TestData::getTestImage2(), 1);
+        for(1..3) {
+            $self->printAddTextLine('');
+        }
+    }
+    
+    for(1..3) {
+        $self->printAddTextLine('');
+    }
+    $self->printAddTextLine('**** END OF TEST ****');
+    for(1..3) {
+        $self->printAddTextLine('');
+    }
+    
+    $self->printEndDocument();
+    return;
+}
+
+sub makeFName {
+    my ($self) = @_;
+    
+    my $fname = '';
+    while($fname eq '') {
+        $fname = '/tmp/posprint_' . $PID . '_';
+        for(1..10) {
+            $fname .= '' . int(rand(10)) . '';
+        }
+        $fname .= '.png';
+    }
+    
+    return $fname;
+}
+
+
+1;
