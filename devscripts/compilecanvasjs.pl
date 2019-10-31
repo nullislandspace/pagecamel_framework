227331d31a9739a68f1f303961e4865c0c8f4da6
Rene Schickbauer <cavac@blackmesa>
2019-10-31 13:13:31 +0100

    Add minified canvasjs library

---
+++ devscripts/compilecanvasjs.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/env perl
+#---AUTOPRAGMASTART---
+use 5.020;
+use strict;
+use warnings;
+use diagnostics;
+use mro 'c3';
+use English qw(-no_match_vars);
+use Carp;
+our $VERSION = 2.3;
+use Fatal qw( close );
+use Array::Contains;
+#---AUTOPRAGMAEND---
+# PAGECAMEL  (C) 2008-2018 Rene Schickbauer
+# Developed under Artistic license
+
+
+use JavaScript::Minifier qw(minify);
+
+my $BASE = "./lib/PageCamel/Web/Static/";
+
+my @files = <DATA>;
+
+unlink($BASE . "canvasjs.compiled.js");
+unlink($BASE . "canvasjs.compiled-min.js");
+print "### Compiling ###\n";
+foreach my $file (@files) {
+    chomp $file;
+    next if($file =~ /^\#/);
+    if(!-f $BASE . $file) {
+        die("File $file not found!");
+    }
+    my $cmd = "cat " . $BASE . $file . " >> " . $BASE . "canvasjs.compiled.js";
+    print "$cmd\n";
+    my @ret = `$cmd`;
+    print @ret;
+}
+print "### Minifying ###\n";
+open(my $ifh, "<", $BASE . "canvasjs.compiled.js") or die($ERRNO);
+open(my $ofh, ">", $BASE . "canvasjs.compiled-min.js") or die($ERRNO);
+
+minify(input => *$ifh, outfile => *$ofh);
+
+close $ifh;
+close $ofh;
+print "Done.\n";
+unlink($BASE . "canvasjs.compiled.js");
+
+print "For development, use the following lines:\n";
+foreach my $file (@files) {
+    next if($file =~ /^\#/);
+    print "<script type=\"text/javascript\" src=\"/static/" . $file . "[% URLReloadPostfix %]\"></script>\n";
+}
+
+__DATA__
+canvasjs/canvashelpers.js
+canvasjs/canvasbuttons.js
+canvasjs/canvas7segment.js
+canvasjs/canvasitemlist.js
