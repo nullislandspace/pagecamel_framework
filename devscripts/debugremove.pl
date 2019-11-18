28fae1ba5a5a43444ecaa6e3435839bdfd18928c
Rene Schickbauer <cavac@cables.cavac.at>
2019-11-18 14:35:48 +0100

    devscript to inject debugstatements for (nearly all) sub calls within the framework

---
+++ devscripts/debugremove.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/env perl
+#---AUTOPRAGMASTART---
+use 5.030;
+use strict;
+use warnings;
+use diagnostics;
+use mro 'c3';
+use English;
+use Carp;
+our $VERSION = 2.4;
+use autodie qw( close );
+use Array::Contains;
+use utf8;
+use Encode qw(is_utf8 encode_utf8 decode_utf8);
+#---AUTOPRAGMAEND---
+
+# PAGECAMEL  (C) 2008-2019 Rene Schickbauer
+# Developed under Artistic license
+
+
+print "Searching files...\n";
+my @files = (find_pm('lib'));
+
+print "Changing files:\n";
+foreach my $file (@files) {
+    my $inserted = 0;
+    print "Editing $file...\n";
+
+    my @lines;
+    open(my $ifh, "<", $file) or die($ERRNO);
+    @lines = <$ifh>;
+    close $ifh;
+
+    open(my $ofh, ">", $file) or die($ERRNO);
+    my $pkgname = '--undef--';
+    foreach my $line (@lines) {
+        if($line =~ /\#\ DEBUGINJECT/) {
+            next;
+        }
+        print $ofh $line;
+    }
+    close $ofh;
+}
+print "Done.\n";
+exit(0);
+
+
+
+sub find_pm {
+    my ($workDir) = @_;
+
+    my @files;
+    opendir(my $dfh, $workDir) or die($ERRNO);
+    while((my $fname = readdir($dfh))) {
+        next if($fname eq "." || $fname eq ".." || $fname eq ".hg");
+        $fname = $workDir . "/" . $fname;
+        if(-d $fname) {
+            push @files, find_pm($fname);
+        } elsif($fname =~ /\.p[lm]$/i && -f $fname) {
+            push @files, $fname;
+        }
+    }
+    closedir($dfh);
+    return @files;
+}
