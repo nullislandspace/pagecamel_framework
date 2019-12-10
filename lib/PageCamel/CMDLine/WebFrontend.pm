28b897ea978f76ee8bd9d96fbad6ecdaa0fc2108
Rene Schickbauer <cavac@rosetta>
2019-12-10 10:52:11 +0100

    Basic layout of CMDLine/WebFrontend.pm
    --HG--
    branch : incoming_protocol_proxy

---
+++ lib/PageCamel/CMDLine/WebFrontend.pm
@@ -0,0 +1,163 @@
+package PageCamel::CMDLine::WebFrontend;
+#---AUTOPRAGMASTART---
+use 5.030;
+use strict;
+use warnings;
+use diagnostics;
+use mro 'c3';
+use English;
+use Carp qw[carp croak confess cluck longmess shortmess];
+our $VERSION = 2.4;
+use autodie qw( close );
+use Array::Contains;
+use utf8;
+use Encode qw(is_utf8 encode_utf8 decode_utf8);
+#---AUTOPRAGMAEND---
+
+use IO::Socket::INET;
+use IO::Socket::SSL;
+use IO::Select;
+use PageCamel::Helpers::ConfigLoader;
+use Time::HiRes qw(sleep usleep);
+use PageCamel::Helpers::Logo;
+use Data::Dumper;
+use Sys::Hostname;
+use POSIX ":sys_wait_h";
+
+# For turning off SSL session cache
+use Readonly;
+Readonly my $SSL_SESS_CACHE_OFF => 0x0000;
+
+my $childcount = 0;
+$SIG{CHLD} = \&REAPER;
+sub REAPER {
+    my $stiff;
+    while (($stiff = waitpid(-1, &WNOHANG)) > 0) {
+        print "Child PID $stiff has gone the way of the Dodo\n";
+        $childcount--;
+    }
+    $SIG{CHLD} = \&REAPER; # install *after* calling waitpid
+}
+
+sub new {
+    my ($class, $isDebugging, $configfile) = @_;
+    my $self = bless {}, $class;
+    
+    $self->{isDebugging} = $isDebugging;
+    $self->{configfile} = $configfile;
+    
+    croak("Config file $configfile not found!") unless(-f $configfile);
+
+    $self->init();
+    
+    return $self;
+}
+
+sub init {
+    my ($self) = @_;
+    
+    print "Loading config file ", $self->{configfile}, "\n";
+    my $config = LoadConfig($self->{configfile},
+                        ForceArray => [ 'service', 'ip'],);
+    
+    $self->{config} = $config;
+
+    my $hname = hostname;
+    if(defined($config->{hosts}->{$hname})) {
+        print "   Host-specific configuration for '$hname'\n";
+        foreach my $keyname (keys %{$config->{hosts}->{$hname}}) {
+            $config->{$keyname} = $config->{hosts}->{$hname}->{$keyname};
+        }
+    }
+    
+    my $APPNAME = $config->{appname};
+    PageCamelLogo($APPNAME, $VERSION);
+    print "Changing application name to '$APPNAME'\n\n";
+    my $ps_appname = lc($APPNAME);
+    $ps_appname =~ s/[^a-z0-9]+/_/gio;
+    
+    if(!-d '/run/lock/pagecamel') {
+        mkdir '/run/lock/pagecamel';
+        chmod 0755, '/run/lock/pagecamel';
+    }
+    
+    my $weblockname = "/run/lock/pagecamel_webgui_frontend.lock";
+
+    if(-f $weblockname) {
+        carp("LOCKFILE $weblockname ALREADY EXISTS!");
+        carp("REMOVING LOCKFILE $weblockname!");
+        unlink $weblockname;
+    }
+
+    # FIXME Add exclusive locked open for $weblockname
+
+
+    $PROGRAM_NAME = $ps_appname;
+
+
+    my @tcpsockets;
+    foreach my $service (@{$config->{external_network}->{service}}) {
+        print '** Service at port ', $service->{port}, ' does ', $service->{usessl} ? '' : 'NOT', " use SSL/TLS\n";
+        foreach my $ip (@{$service->{bind_adresses}->{ip}}) {
+            my $tcp = IO::Socket::IP->new(
+                    LocalHost => $ip,
+                    LocalPort => $service->{port},
+                    Listen => 1,
+                    ReuseAddr => 1,
+                    Proto => 'tcp',
+                ) or croak($ERRNO);
+                #binmode($tcp, ':bytes');
+                push @tcpsockets, $tcp;
+                print "   Listening on $ip:", $config->{port}, "/tcp\n";
+        }
+    }
+    my $select = IO::Select->new(@tcpsockets);
+    $self->{select} = $select;
+        
+    
+    return;
+}
+
+sub run {
+    my ($self) = @_;
+
+    while((my @connections = $self->{select}->can_read)) {
+        foreach my $connection (@connections) {
+            my $client = $connection->accept;
+
+            if($childcount >= $self->{config}->{max_childs}) {
+                print "Too many children already!\n";
+                $client->close;
+                next;
+            }
+
+            my $childpid = fork();
+            if(!defined($childpid)) {
+                print "FORK FAILED!\n";
+                $client->close;
+                next;
+            } elsif($childpid == 0) {
+                # Child
+                print "Doing some network stuff in PID $PID\n";
+                $self->handleClient($client);
+                print "Child PID $PID is done, exiting...\n";
+                exit(0);
+            } else {
+                # Parent
+                $childcount++;
+                next;
+            }
+        }
+    }
+
+    print "run() loop finished.\n";
+    return;
+}
+
+sub handleClient {
+    my ($self, $client) = @_;
+
+    print "Doing some network stuff in child PID $PID\n";
+
+    return;
+}
