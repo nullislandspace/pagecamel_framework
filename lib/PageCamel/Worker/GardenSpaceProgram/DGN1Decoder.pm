9a32ae88d1d31d979c017ac618fbb367a7022649
Rene Schickbauer <cavac@venera>
2020-10-12 09:54:00 +0200

    BF

--- lib/PageCamel/Worker/GardenSpaceProgram/DGN1Decoder.pm
+++ lib/PageCamel/Worker/GardenSpaceProgram/DGN1Decoder.pm
@@ -21,6 +21,7 @@ use Data::Dumper;
 use base qw(PageCamel::Worker::BaseModule);
 use Net::Clacks::Client;
 use PageCamel::Helpers::DateStrings;
+use PageCamel::Helpers::Padding qw[doFPad];
 
 sub new {
     my ($proto, %config) = @_;
@@ -63,15 +64,9 @@ sub crossregister {
 sub work {
     my ($self) = @_;
 
-    my $dbh = $self->{server}->{modules}->{$self->{db}};
     my $reph = $self->{server}->{modules}->{$self->{reporting}};
 
     my $workCount = 0;
-
-    my $insth = $dbh->prepare_cached("INSERT INTO gsp.dgn1_status
-                                        (ticks, memfree, packets_in, packets_out)
-                                        VALUES (?, ?, ?, ?)")
-            or croak($dbh->errstr);
 
     my $now = time;
     if($now > $self->{nextping}) {
@@ -91,23 +86,11 @@ sub work {
             next;
         }
         next unless($message->{type} eq 'set');
+
         next unless($message->{name} eq 'GSP::RECIEVE::AC');
 
-        my ($ok, $decoded) = $self->decodeFrame($message->{data});
+        my ($ok) = $self->decodeFrame($message->{data});
 
-        next unless($ok);
-
-        if(!$insth->execute(
-                    $decoded->{ticks},
-                    $decoded->{memfree},
-                    $decoded->{packets_in},
-                    $decoded->{packets_out},
-            )) {
-            $reph->debuglog("DB ERROR: " . $dbh->errstr);
-            $dbh->rollback;
-        } else {
-            $dbh->commit;
-        }
     }
 
     return $workCount;
@@ -130,35 +113,77 @@ sub decodeFrame {
         push @frame, $val;
     }
 
-    #if($frame[0] != 0x02) {
-    #    # Not directly from DGN1
-    #    # (Linksender does not match)
-    #    # We ignore non-direct frames here
-    #    return 0;
-    #}
-
+    $reph->debuglog("Frame type ", $frame[8], " from ", $frame[6], " recieved!");
     if($frame[6] != 0x02) {
         # Not originating from DGN1
         return 0;
     }
 
-    if($frame[8] != 1) {
-        # Not a status frame
-        return 0;
+    if($frame[8] == 1) {
+        return $self->decodeStatusFrame(@frame);
+    } elsif($frame[8] == 11) {
+        return $self->decodeDHTFrame(@frame);
+    } elsif($frame[8] == 33) {
+        # Wake up from powersave
+        my %decoded;
+        $decoded{statechange_timestamp} = getISODate();
+        $decoded{statechange_text} = 'WakeUp from PowerSave';
+        my @parts;
+        foreach my $key (sort keys %decoded) {
+            push @parts, $key . '=' . $decoded{$key};
+        }
+        my $clacksdata = join(',', @parts);
+        $self->{clacks}->set('GSP::DGN1::STATECHANGE', $clacksdata);
+    } elsif($frame[8] == 36) {
+        # powersave shutdown
+        my %decoded;
+        $decoded{statechange_timestamp} = getISODate();
+        $decoded{statechange_text} = 'Shutdown for PowerSave';
+        my @parts;
+        foreach my $key (sort keys %decoded) {
+            push @parts, $key . '=' . $decoded{$key};
+        }
+        my $clacksdata = join(',', @parts);
+        $self->{clacks}->set('GSP::DGN1::STATECHANGE', $clacksdata);
+    } elsif($frame[8] == 19) {
+        return $self->decodeSoilCapacityData(@frame);
+    } elsif($frame[8] == 32) {
+        return $self->decodeRFISeriesData(@frame);
     }
+    return;
+}
 
 
+sub decodeStatusFrame {
+    my ($self, @frame) =@_;
+    
     my $data = 12;
+    my $dbh = $self->{server}->{modules}->{$self->{db}};
+    my $reph = $self->{server}->{modules}->{$self->{reporting}};
+
+    my $insth = $dbh->prepare_cached("INSERT INTO gsp.dgn1_status
+                                        (ticks, vcc, packets_in, packets_out, firmware_version, keepawake_enabled, powersave_enabled)
+                                        VALUES (?, ?, ?, ?, ?, ?, ?)")
+            or croak($dbh->errstr);
+
 
     my %decoded;
     $decoded{ticks} = ($frame[$data + 0] << 24) + ($frame[$data + 1] << 16) + ($frame[$data + 2] << 8) + $frame[$data + 3];
+    $decoded{vcc} = ($frame[$data + 6] << 8) + $frame[$data + 13];
 
-    $decoded{memfree} = ($frame[$data + 4] << 24) + ($frame[$data + 5] << 16) + ($frame[$data + 6] << 8) + $frame[$data + 7];
 
     $decoded{packets_in} = ($frame[$data + 12] << 8) + $frame[$data + 13];
     $decoded{packets_out} = ($frame[$data + 14] << 8) + $frame[$data + 15];
 
-    $decoded{timestamp} = getISODate();
+    $decoded{keepawake_enabled} = $frame[$data + 4];
+    $decoded{powersave_enabled} = $frame[$data + 5];
+
+    my $vcc = (($frame[$data + 6] << 8) + $frame[$data + 7])/1000;
+    $decoded{vcc} = $vcc;
+
+    $decoded{firmware_version} = $frame[$data + 10];
+
+    $decoded{status_timestamp} = getISODate();
     my @parts;
     foreach my $key (sort keys %decoded) {
         push @parts, $key . '=' . $decoded{$key};
@@ -168,10 +193,112 @@ sub decodeFrame {
     $self->{clacks}->doNetwork();
     $reph->debuglog("DGN1 status frame: $clacksdata");
 
+    if(!$insth->execute(
+                $decoded{ticks},
+                $decoded{vcc},
+                $decoded{packets_in},
+                $decoded{packets_out},
+                $decoded{firmware_version},
+                $decoded{keepawake_enabled},
+                $decoded{powersave_enabled},
+        )) {
+        $reph->debuglog("DB ERROR: " . $dbh->errstr);
+        $dbh->rollback;
+        return 0;
+    }
 
-    return (1, \%decoded);
+    $dbh->commit;
+    return 1;
 }
 
+sub decodeDHTFrame {
+    my ($self, @frame) =@_;
+    
+    my $data = 12;
+    my $dbh = $self->{server}->{modules}->{$self->{db}};
+    my $reph = $self->{server}->{modules}->{$self->{reporting}};
+
+    my $insth = $dbh->prepare_cached("INSERT INTO gsp.dgn1_dht
+                                        (status, temperature, humidity)
+                                        VALUES (?, ?, ?)")
+            or croak($dbh->errstr);
+
+
+    my %decoded;
+
+    $decoded{status} = 'UNKNOWN_ERROR';
+    $decoded{temperature} = '999';
+    $decoded{humidity} = '999';
+    if($frame[$data + 0] == 0x00) {
+        $decoded{status} = 'OK';
+        $decoded{humidity} = ($frame[$data + 1] << 8) + $frame[$data + 2];
+        $decoded{temperature} = ($frame[$data + 3] << 8) + $frame[$data + 4];
+
+        # Convert back to original values
+        $decoded{humidity} = $decoded{humidity} / 10;
+        $decoded{temperature} = ($decoded{temperature} / 10) - 100;
+
+        # Round
+        $decoded{humidity} = $self->roundFloat($decoded{humidity}, 2);
+        $decoded{temperature} = $self->roundFloat($decoded{temperature}, 2);
+    } elsif($frame[$data + 0] == 0x01) {
+        $decoded{status} = 'CHECKSUM_ERROR';
+    } elsif($frame[$data + 0] == 0x02) {
+        $decoded{status} = 'TIMEOUT_ERROR';
+    } else {
+        $decoded{status} = 'OTHER_ERROR';
+    }
+
+    $decoded{dht_timestamp} = getISODate();
+    my @parts;
+    foreach my $key (sort keys %decoded) {
+        push @parts, $key . '=' . $decoded{$key};
+    }
+    my $clacksdata = join(',', @parts);
+    $self->{clacks}->set('GSP::DGN1::DHT', $clacksdata);
+    $self->{clacks}->setAndStore('GSP::DGN1::TEMPERATURE', 0 + $decoded{temperature});
+    $self->{clacks}->setAndStore('GSP::DGN1::HUMIDITY', 0 + $decoded{humidity});
+    $self->{clacks}->doNetwork();
+    $reph->debuglog("DGN1 DHT frame: $clacksdata");
+
+    if(!$insth->execute(
+                $decoded{status},
+                $decoded{temperature},
+                $decoded{humidity},
+        )) {
+        $reph->debuglog("DB ERROR: " . $dbh->errstr);
+        $dbh->rollback;
+        return 0;
+    }
+    
+    $dbh->commit;
+    return 1;
+}
+
+
+sub roundFloat {
+    my ($self, $val, $digits) = @_;
+
+    my $factor = 10**$digits;
+
+    $val = int($val * $factor) / $factor;
+
+    if($val =~ /\./) {
+        my ($pre, $post) = split/\./, $val;
+        while(length($post) < $digits) {
+            $post .= '0';
+        }
+        $val = $pre . '.' . $post;
+    } else {
+        my $post = '';
+        while(length($post) < $digits) {
+            $post .= '0';
+        }
+        $val .= '.' . $post;
+    }
+
+    return $val;
+}
 
 1;
 __END__
