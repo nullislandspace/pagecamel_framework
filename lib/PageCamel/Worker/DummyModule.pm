75f5a178fd3d343bd773301d14a2eb9bb021cb7f
Rene Schickbauer <cavac@earthrise>
2022-01-18 13:57:44 +0100

    Ooops, add missing file

---
+++ lib/PageCamel/Worker/DummyModule.pm
@@ -0,0 +1,104 @@
+package PageCamel::Worker::DummyModule;
+#---AUTOPRAGMASTART---
+use 5.030;
+use strict;
+use warnings;
+use diagnostics;
+use mro 'c3';
+use English;
+use Carp qw[carp croak confess cluck longmess shortmess];
+our $VERSION = 4.0;
+use autodie qw( close );
+use Array::Contains;
+use utf8;
+use Data::Dumper;
+use PageCamel::Helpers::UTF;
+#---AUTOPRAGMAEND---
+
+use base qw(PageCamel::Worker::BaseModule);
+
+sub new {
+    my ($proto, %config) = @_;
+    my $class = ref($proto) || $proto;
+
+    my $self = $class->SUPER::new(%config); # Call parent NEW
+    bless $self, $class; # Re-bless with our class
+
+    $self->{lastRun} = "";
+
+    return $self;
+}
+
+
+sub register {
+    my $self = shift;
+    $self->register_worker("work");
+    return;
+}
+
+
+sub work {
+    my ($self) = @_;
+
+    my $reph = $self->{server}->{modules}->{$self->{reporting}};
+
+    my $workCount = 0;
+
+    $reph->debuglog("Hello World");
+
+    return $workCount;
+}
+
+
+1;
+__END__
+
+=head1 NAME
+
+PageCamel::Worker::TableStatistics -
+
+=head1 SYNOPSIS
+
+  use PageCamel::Worker::TableStatistics;
+
+
+
+=head1 DESCRIPTION
+
+
+
+=head2 new
+
+
+
+=head2 register
+
+
+
+=head2 work
+
+
+
+=head1 IMPORTANT NOTE
+
+This module is part of the PageCamel framework. Currently, only limited support
+and documentation exists outside my DarkPAN repositories. This source is
+currently only provided for your reference and usage in other projects (just
+copy&paste what you need, see license terms below).
+
+To see PageCamel in action and for news about the project,
+visit my blog at L<https://cavac.at>.
+
+=head1 AUTHOR
+
+Rene Schickbauer, E<lt>pagecamel@cavac.atE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2008-2020 Rene Schickbauer
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
