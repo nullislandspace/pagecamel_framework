c843b9979498d8f7a725dc0a4d5fd804637cead8
Rene Schickbauer <cavac@cables.cavac.at>
2022-06-23 12:49:32 +0200

    BF, check for optional args

--- devscripts/subsignatures.pl
+++ devscripts/subsignatures.pl
@@ -23,6 +23,13 @@ use PageCamel::Helpers::UTF;
 print "Searching files...\n";
 my @files = (find_pm('devscripts'), find_pm('lib'));
 
+#@files = ('lib/PageCamel/Helpers/PostgresDB.pm');
+
+die('optionalargs.txt already exists!') if(-f 'optionalargs.txt');
+open(my $optfh, '>', 'optionalargs.txt') or die($ERRNO);
+
+my %knownoptionals;
+
 print "Changing files:\n";
 foreach my $file (@files) {
     my $inserted = 0;
@@ -33,35 +40,106 @@ foreach my $file (@files) {
     @lines = <$ifh>;
     close $ifh;
 
+    #open(my $ofh, ">", 'bla') or die($ERRNO);
     open(my $ofh, ">", $file) or die($ERRNO);
     while(scalar @lines) {
         my $line = shift @lines;
         my ($subname, $args);
-        if($line =~ /^sub\ (.*)\ \{/) {
+        if($line =~ /[^\#]*sub\ (.*)\ \{/) {
             $subname = $1;
         } else {
             print $ofh $line;
             next;
         }
-        if($lines[0] =~ /my\ .*?\((.*)\).*\@\_/) {
+        if($lines[0] =~ /[^\#]*my\ .*?\((.*)\).*\@\_/) {
             $args = $1;
+        } elsif($lines[0] =~ /[^\#]*my\ \(*(.*?)\)*\ *\=\ *shift(\ *\@\_)*/) {
+            $args = $1;
+            print "-------------- SHIFT ARG!!! ---------------\n";
+            print "     $args\n";
         } else {
             print "#### Sub $subname has no args\n";
             print $ofh $line;
             next;
         }
 
+        $subname =~ s/^\ +//g;
+        $subname =~ s/\ +$//g;
+        $args =~ s/^\ +//g;
+        $args =~ s/\ +$//g;
+
         my $temp = shift @lines;
         my $newsub = 'sub ' . $subname . '(' . $args . ') {' . "\n";
         print $newsub;
         print $ofh $newsub;
+
+        lookForOptionals($file, $subname, $args, @lines);
     }
     close $ofh;
 }
+
+close $optfh;
 print "Done.\n";
 exit(0);
 
+sub lookForOptionals($file, $subname, $arglist, @lines) {
+    my @sublines = getSublines(@lines);
+    my @args = getArgs($arglist);
 
+    foreach my $arg (@args) {
+        foreach my $line (@sublines) {
+            my $matcharg = 'defined\(\ *\\' . $arg . '\ *\)';
+            if($line =~ /$matcharg/) {
+                my $key = join('___', $file, $subname, $arg);
+                if(!defined($knownoptionals{$key})) {
+                    $knownoptionals{$key} = 1;
+                    print "$file / $subname: Optional argument $arg\n";
+                    print $optfh "$file / $subname: Optional argument $arg\n";
+                }
+            }
+        }
+    }
+
+    return;
+}
+
+sub getSublines(@lines) {
+    my $count = 1;
+
+    my @sublines;
+
+    for(my $i = 0; $i < scalar @lines; $i++) {
+        my $line = $lines[$i];
+        push @sublines, $line;
+        $count += getBraceCount($line);
+        last unless($count);
+    }
+
+    return @sublines;
+}
+
+sub getBraceCount($line) {
+    my $count = 0;
+    my @parts = split//, $line;
+    foreach my $part (@parts) {
+        if($part eq '{') {
+            $count++;
+        } elsif($part eq '}') {
+            $count--;
+        }
+    }
+    return $count;
+}
+
+sub getArgs($arglist) {
+    my @args = split/\,/, $arglist;
+
+    for(my $i = 0; $i < scalar @args; $i++) {
+        $args[$i] =~ s/^\ +//;
+        $args[$i] =~ s/\ +$//;
+    }
+    return @args;
+}
 
 sub find_pm($workDir) {
 
