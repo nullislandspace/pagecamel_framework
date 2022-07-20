7e303332ebb84f06cf3dbc143ab8441e162264a4
Rene Schickbauer <cavac@cables.cavac.at>
2022-07-20 13:27:52 +0200

    Compile canvasuijs

--- devscripts/compilecanvasuijs.pl
+++ devscripts/compilecanvasuijs.pl
@@ -74,24 +74,23 @@ foreach my $file (@files) {
 }
 
 __DATA__
-#canvasuijs/trhelper.js !! Dummy function for development only
-canvasuijs/uiview.js
-canvasuijs/uitext.js
-canvasuijs/uibutton.js
+canvasuijs/trhelper.js
 canvasuijs/canvashelpers.js
+canvasuijs/uiview.js
+canvasuijs/uitextbox.js
+canvasuijs/uitext.js
 canvasuijs/uiline.js
 canvasuijs/uilist.js
 canvasuijs/uinumpad.js
 canvasuijs/uiarrowbutton.js
 canvasuijs/uibuttonrow.js
-canvasuijs/uidialog.js
-canvasuijs/uitextbox.js
-canvasuijs/uipaylist.js
+canvasuijs/uiscrolllist.js
 canvasuijs/uilistitem.js
 canvasuijs/uidragndrop.js
-canvasuijs/uitableplan.js
 canvasuijs/uicircle.js
 canvasuijs/uicolorpalet.js
 canvasuijs/uiimage.js
 canvasuijs/uicheckbox.js
 canvasuijs/uitextinput.js
+canvasuijs/uibutton.js
+canvasuijs/uidialog.js
