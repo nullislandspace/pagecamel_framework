dd5da0b1567589d12235658ba078573ba61b8cf6
Tobias Orthofer <orthofer@palebluedot>
2023-11-15 08:26:00 +0100

    changed module resolution
    --HG--
    branch : orthofer

--- lib/PageCamel/Web/Static/pchelpers/out/SQLtoBinWorker.d.ts
+++ lib/PageCamel/Web/Static/pchelpers/out/SQLtoBinWorker.d.ts
@@ -1,1 +1,8 @@
-export {};
+declare var nextSave: Uint8Array;
+declare var hasnextSave: boolean;
+declare var intervalhandler: any;
+declare var LZString: {
+    compress: (data: string) => string;
+    decompress: (data: string) => string;
+};
+declare function dataConverter(): void;
