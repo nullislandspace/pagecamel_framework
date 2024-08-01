3c0221eb31fbb86bf78ea2e716bb821c2c5ec46a
Rene Schickbauer <cavac@palebluedot>
2024-08-01 09:31:25 +0200

    Make sure install_patched_modules.sh also installs all required dependencies

--- devscripts/install_patched_modules.sh
+++ devscripts/install_patched_modules.sh
@@ -3,15 +3,10 @@ wget https://cavac.at/public/pimenu/download/Image-Imlib2-2.04.tar.gz &&
 tar xvzf Net-CUPS2-0.65.tar.gz &&
 tar xvzf Image-Imlib2-2.04.tar.gz &&
 cd Net-CUPS2-0.65 &&
-perl Makefile.PL &&
-make test install distclean &&
+cpan .
 cd .. &&
 cd Image-Imlib2-2.04 &&
-perl Build.PL &&
-./Build &&
-./Build test &&
-./Build install &&
-./Build distclean &&
+cpan .
 cd .. &&
 rm -rf Net-CUPS2-0.65.tar.gz Net-CUPS2-0.65 Image-Imlib2-2.04.tar.gz Image-Imlib2-2.04 &&
 echo DONE
