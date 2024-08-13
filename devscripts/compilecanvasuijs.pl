97c76dbc0b5c1830962c0f8a7d774e6d706e5cc7
Rene Schickbauer <cavac@palebluedot>
2024-08-13 13:25:44 +0200

    Bump version to 4.5
    --HG--
    branch : _feature_removets

--- devscripts/compilecanvasuijs.pl
+++ devscripts/compilecanvasuijs.pl
@@ -6,7 +6,7 @@ use diagnostics;
 use mro 'c3';
 use English;
 use Carp qw[carp croak confess cluck longmess shortmess];
-our $VERSION = 4.4;
+our $VERSION = 4.5;
 use autodie qw( close );
 use Array::Contains;
 use utf8;
