0950b4d17e077696f91f2cd8f5ef31296433e0b8
Rene Schickbauer <cavac@palebluedot>
2024-11-28 13:30:51 +0100

    Add ability to hide internal organisations

--- database/0009_hide_developer_users.sql
+++ database/0009_hide_developer_users.sql
@@ -5,3 +5,6 @@ UPDATE pagecamel.users SET is_internal = true WHERE username IN('cashbook', 'adm
 ALTER TABLE pagecamel.permissiongroups ADD COLUMN is_internal boolean NOT NULL DEFAULT false;
 UPDATE pagecamel.permissiongroups SET is_internal = true WHERE groupname IN ('Developer');
 
+ALTER TABLE pagecamel.users_organisation ADD COLUMN is_internal boolean NOT NULL DEFAULT false;
+UPDATE pagecamel.users_organisation SET is_internal = true WHERE organisation_name IN ('Developer', 'Calyx_Automation');
+
